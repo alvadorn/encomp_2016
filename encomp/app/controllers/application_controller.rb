5257a79a11fffe4bfc879a7eb733c66b80e336a8
Igor Sant'ana <igorba2005@gmail.com>
2016-08-08 19:47:28 -0300

    Fine

--- encomp/app/controllers/application_controller.rb
+++ encomp/app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
     admin_index_path
+  end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "try_login"
+    else
+      "application"
+    end
   end
 end
--- encomp/app/controllers/users/sessions_controller.rb
+++ encomp/app/controllers/users/sessions_controller.rb
@@ -3,6 +3,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # GET /resource/sign_in
   def new
+    layout: "admin"
      super
   end
 
