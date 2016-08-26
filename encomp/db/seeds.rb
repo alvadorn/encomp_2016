ea07a6d2aa1e37124070613b9d5649e300f45e96
Igor Sant'ana <igorba2005@gmail.com>
2016-08-26 08:54:26 -0300

    Seeding data

--- encomp/db/seeds.rb
+++ encomp/db/seeds.rb
@@ -6,6 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-igor = User.new(name: "Igor Sant'ana", email: "igorba2005@gmail.com", password: "123456")
-igor.skip_confirmation!
-igor.save!
+igor = User.new(name: "Igor Sant'ana", email: "igorba2005@gmail.com", password: "123456", admin: true)
+igor.save
+encomp = User.new(name: "Encomp Oficial", email: "contato@encomp.com.br", password: "encomp_oficial2016!", admin: true)
+encomp.save
+
+Course.create([
+  {name: "Ruby On Rails", desc: "", day: Date.new(2016, 9, 20)},
+  {name: "Git", desc: "", day: Date.new(2016, 9, 20)},
+  {name: "Robótica", desc: "", day: Date.new(2016,9,22)},
+  {name: "Aprendizado de Máquina", desc: "", day: Date.new(2016,9,22)},
+  {name: "Linux", desc: "", day: Date.new(2016,9,22)},
+  ])
