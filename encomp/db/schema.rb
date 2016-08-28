ee4cdc8690e6c1801f73c3acc7814f9dba908b84
Igor Sant'ana <igorba2005@gmail.com>
2016-08-28 13:38:26 -0300

    Doing some changes

--- encomp/db/schema.rb
+++ encomp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815013843) do
+ActiveRecord::Schema.define(version: 20160828161738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20160815013843) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.date     "day",                     null: false
+  end
+
+  create_table "courses_users", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "course_id"
   end
 
   create_table "lectures", force: :cascade do |t|
