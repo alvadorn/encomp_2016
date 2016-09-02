3d3d4380f3ab912b586d7a389c873c2dcf445ffb
Igor Sant'ana <igorba2005@gmail.com>
2016-09-01 23:02:26 -0300

    Changing team

--- encomp/app/assets/javascripts/user-create.js
+++ encomp/app/assets/javascripts/user-create.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
   changeHidden();
   changeHiddenDeposit();
-  $('[data-toggle="tooltip"]').tooltip(); 
+  changeHidden2();
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
 $("#minicourses input[type=checkbox]").click(function() {
   var checkboxSwap = $("#minicourses input:checkbox.change");
   changeCheckBox(checkboxSwap, this);
   changeHidden();
+  changeHidden2();
 });
 
 $("#payment_preference_choose input[type=radio]").click(function() {
@@ -17,6 +19,16 @@ $("#payment_preference_choose input[type=radio]").click(function() {
 var changeHidden = function() {
   var checkMaratona = $("#user_courses_torneio_de_programao");
   var field = $("#user_team_name");
+  if (checkMaratona.is(":checked")) {
+    field[0].style.display = "inline";
+  } else {
+    field[0].style.display = "none";
+  }
+}
+
+var changeHidden2 = function() {
+  var checkMaratona = $("#user_courses_visita_tcnica");
+  var field = $("#visita-tecnica-form");
   if (checkMaratona.is(":checked")) {
     field[0].style.display = "inline";
   } else {
