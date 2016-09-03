c79822367be1bdafea05e16b52bba1d778b6f468
Igor Sant'ana <igorba2005@gmail.com>
2016-09-03 16:04:05 -0300

    Adding admin page

--- encomp/app/controllers/home_controller.rb
+++ encomp/app/controllers/home_controller.rb
@@ -1,13 +1,22 @@
 class HomeController < AdminController
   before_action :authenticate_user!, only: :admin_index
+  before_action :set_user, only: :admin_index
   before_action :permission!, only: :admin_index
 
   def index
   end
 
   def admin_index
-    @user = current_user
+    #@user = current_user
+    @inscripted_paid = User.where("admin = false and paid = true").count
+    @inscripted_count = User.where("admin = false and paid = false").count
+    @courses = Course.select(:name).all
     render layout: "admin"
+  end
+
+  private
+  def set_user
+    @user = current_user
   end
 
 end
