280cfea6f87d45198e443934e3571b84f457fdab
Igor Sant'ana <igorba2005@gmail.com>
2016-09-05 09:05:36 -0300

    Modifying system

--- encomp/app/controllers/home_controller.rb
+++ encomp/app/controllers/home_controller.rb
@@ -15,6 +15,8 @@ class HomeController < AdminController
     @male = User.where("accomodation = 'Masculino'").count
     @female = User.where("accomodation = 'Feminino'").count
     @mixed = User.where("accomodation = 'Misto'").count
+    @opt1 = User.where("payment_preference = 1 and (admin = false and auxiliar = false)").count
+    @opt2 = User.where("payment_preference = 2 and (admin = false and auxiliar = false)").count
     render layout: "admin"
   end
 
