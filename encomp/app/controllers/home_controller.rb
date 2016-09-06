2ff2eb9becc1825787f48c688ebe6f2644d34641
Igor Sant'ana <igorba2005@gmail.com>
2016-09-06 13:49:20 -0300

    Subscriber count

--- encomp/app/controllers/home_controller.rb
+++ encomp/app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < AdminController
   def admin_index
     #@user = current_user
     @inscripted_paid = User.where("admin = false and paid = true").count
-    @inscripted_count = User.where("admin = false and paid = false").count
+    @inscripted_count = User.where("admin = false and auxiliar = false").count
     @courses = Course.all
     @no_accomodation = User.where("accomodation = 'Não necessário'").count
     @male = User.where("accomodation = 'Masculino'").count
