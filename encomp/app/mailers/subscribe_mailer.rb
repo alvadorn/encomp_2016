83ae1b6115397e61c260291cfc492b10fb60433d
Igor Sant'ana <igorba2005@gmail.com>
2016-09-09 14:33:15 -0300

    Test email

--- encomp/app/mailers/subscribe_mailer.rb
+++ encomp/app/mailers/subscribe_mailer.rb
@@ -9,4 +9,8 @@ class SubscribeMailer < ApplicationMailer
     @user = user
     mail(to: @user.email, subject: "Inscrição confirmada com sucesso!")
   end
+
+  def test_email(mail)
+    mail(to: mail, subject: "test mail")
+  end
 end
